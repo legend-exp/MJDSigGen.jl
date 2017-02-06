516733900d89861f271af8b05335d1b709a3dcf9
Oliver Schulz <oschulz@mpp.mpg.de>
2017-02-06 16:12:13 +0100

    Add more wrappers for siggen functions
    Also, for now, don't export wrapper functions.

--- src/wrappers.jl
+++ src/wrappers.jl
@@ -1,21 +1,31 @@
 # This file is a part of MJDSigGen, licensed under the MIT License (MIT).
 
-export signal_calc_init
-export get_signal, get_signal!
-
-
-function signal_calc_init(config_filename::AbstractString)
-     setup = Struct_MJD_Siggen_Setup()
-
+function read_config!(setup::Struct_MJD_Siggen_Setup, config_filename::AbstractString)
     r = ccall(
-        @sgsym(:signal_calc_init),
-        Cint,
+        @sgsym(:read_config), Cint,
+        (Cstring, Ptr{Struct_MJD_Siggen_Setup}),
+        pointer(config_filename), Ref(setup)
+    )
+    r != 0 && error("read_config failed.")
+    setup
+end
+
+read_config(config_filename::AbstractString) =
+    read_config!(Struct_MJD_Siggen_Setup(), config_filename)
+
+
+function signal_calc_init!(setup::Struct_MJD_Siggen_Setup, config_filename::AbstractString)
+    r = ccall(
+        @sgsym(:signal_calc_init), Cint,
         (Cstring, Ptr{Struct_MJD_Siggen_Setup}),
         pointer(config_filename), Ref(setup)
     )
     r != 0 && error("signal_calc_init failed.")
     setup
 end
+
+signal_calc_init(config_filename::AbstractString) =
+    signal_calc_init!(Struct_MJD_Siggen_Setup(), config_filename)
 
 
 function get_signal!(signal, setup::Struct_MJD_Siggen_Setup, location::NTuple{3})
@@ -24,8 +34,7 @@ function get_signal!(signal, setup::Struct_MJD_Siggen_Setup, location::NTuple{3}
     pt = Struct_point(location[1], location[2], location[3])
 
     r = ccall(
-        @sgsym(:get_signal),
-        Cint,
+        @sgsym(:get_signal), Cint,
         (Struct_point, Ptr{Float32}, Ptr{Struct_MJD_Siggen_Setup}),
         pt, signal, Ref(setup)
     )
@@ -37,3 +46,16 @@ end
 
 get_signal(setup::Struct_MJD_Siggen_Setup, location::NTuple{3}) =
     get_signal!(zeros(Float32, setup.ntsteps_out), setup, location)
+
+
+function outside_detector(setup::Struct_MJD_Siggen_Setup, location::NTuple{3})
+    pt = Struct_point(location[1], location[2], location[3])
+
+    r = ccall(
+        @sgsym(:outside_detector), Cint,
+        (Struct_point, Ptr{Struct_MJD_Siggen_Setup}),
+        pt, Ref(setup)
+    )
+
+    r != 0
+end
