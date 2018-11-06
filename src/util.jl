a778661ce01a915cd5dd1054761ed02ae07956bc
Lukas <lhauert@mpp.mpg.de>
2018-11-06 12:47:03 +0100

    assert() -> @assert

--- src/util.jl
+++ src/util.jl
@@ -20,7 +20,7 @@ function read_fields(setup::Struct_MJD_Siggen_Setup)
     wpot_data = readdlm(wpot_file_name(setup), comments=true)
     n_r = setup.rlen
     n_z = setup.zlen
-    assert(size(field_data, 1) == size(wpot_data, 1) == n_r * n_z)
+    @assert (size(field_data, 1) == size(wpot_data, 1) == n_r * n_z)
 
     E_pot = copy(reshape(view(field_data, :, 3), (n_z, n_r)))
     W_pot = copy(reshape(view(wpot_data, :, 3), (n_z, n_r)))
