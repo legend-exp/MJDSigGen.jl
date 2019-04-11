43f0ae223feb265f7c8a3c4e942b7554f3fe8bef
Comellato <tommaso.comellato@tum.de>
2019-04-11 16:10:05 +0200

    Bug correction

--- deps/src/mjd_siggen/fields.c
+++ deps/src/mjd_siggen/fields.c
@@ -541,7 +541,7 @@ static int setup_efield(MJD_Siggen_Setup *setup){
     cyl.phi = 0;
     if (outside_detector_cyl(cyl, setup)) continue;
     if(cyl.r>(setup->xtal_radius-2)) {
-		f = - 0.5 * (float)(cyl.r + setup->xtal_radius);
+		f = 0.5 * (float)(setup->xtal_radius-cyl.r);
 		efld[i][j].r = (float)er * f;
 	    efld[i][j].z = (float)ez * f;
 	    efld[i][j].phi = 0;
