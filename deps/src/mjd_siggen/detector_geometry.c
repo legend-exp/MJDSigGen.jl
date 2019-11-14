a05646b4186bc19f0c74038421130370a014c547
Tommaso Comellato <tommaso.comellato@tum.de>
2019-11-14 14:02:58 +0100

    bug fix

--- deps/src/mjd_siggen/detector_geometry.c
+++ deps/src/mjd_siggen/detector_geometry.c
@@ -57,7 +57,7 @@ int outside_detector(point pt, MJD_Siggen_Setup *setup){
 /* check inner taper of hole */
   if (setup->inner_taper_length > 0 &&
       z > setup->zmax - setup->inner_taper_length &&
-      r > setup->hole_radius + ((z - setup->zmax + setup->inner_taper_length) *
+      r < setup->hole_radius + ((z - setup->zmax + setup->inner_taper_length) *
                          (setup->rmax-setup->inner_taper_width-setup->hole_radius) /
                           setup->inner_taper_length)) return 1;
 
@@ -106,7 +106,7 @@ int outside_detector_cyl(cyl_pt pt, MJD_Siggen_Setup *setup){
 /* check inner taper of hole */
   if (setup->inner_taper_length > 0 &&
       z > setup->zmax - setup->inner_taper_length &&
-      r > setup->hole_radius + ((z - setup->zmax + setup->inner_taper_length) *
+      r < setup->hole_radius + ((z - setup->zmax + setup->inner_taper_length) *
                          (setup->rmax-setup->inner_taper_width-setup->hole_radius) /
                           setup->inner_taper_length)) return 1;
 
