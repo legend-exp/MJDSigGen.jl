c19d430ce4ac021298a0db87fbadf97c4495fe8e
ga53sog <tommaso.comellato@tum.de>
2023-10-23 12:04:39 +0200

    test commit

--- deps/src/mjd_siggen/fields.c
+++ deps/src/mjd_siggen/fields.c
@@ -77,11 +77,11 @@ if (setup_velo(setup) != 0){
     return -1;
   }
 
-  if (setup_depletion(setup) != 0){
-    error("Failed to read depletion voltage, config file: %s\n",
-	  setup->config_file_name);
-    return -1;
-  }
+//  if (setup_depletion(setup) != 0){
+//    error("Failed to read depletion voltage, config file: %s\n",
+//	  setup->config_file_name);
+//    return -1;
+//  }
 
   return 0;
 }
